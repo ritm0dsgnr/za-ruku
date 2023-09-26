bc02f279b3b69faea1ffd95250a18b241f305aae
ritm0dsgnr <92969446+ritm0dsgnr@users.noreply.github.com>
2023-09-26 14:58:20 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,74 +5,47 @@ const videoItem = document.querySelectorAll('.video__content-item-video'),
       mask = document.querySelector('.mask')
 
 
+videoItem.forEach(el => {
+  el.addEventListener('click', function () {
+    mask.classList.remove('hidden')
+    popup.classList.remove('hidden')
+  })
+})
+
+videoItem[0].addEventListener('click', function () {
+  if (this.classList.contains('active')){
+    return
+  }
+  this.classList.add('active')
+  popupTitle.innerHTML = 'Лечение пациентов <nobr>с раком</nobr> мочевого пузыря'
+  popup.insertAdjacentHTML('beforeEnd', '<iframe src="https://www.youtube.com/embed/aqGwO4o1sks?si=ctc45fMah1QSuPg7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+})
+
+videoItem[1].addEventListener('click', function () {
+  if (this.classList.contains('active')){
+    return
+  }
+  this.classList.add('active')
+  popupTitle.innerHTML = 'Жизнь пациента <nobr>с раком</nobr> мочевого пузыря после противоопухолевого лечения или во время него'
+  popup.insertAdjacentHTML('beforeEnd', '<iframe src="https://www.youtube.com/embed/4wkZtHJnzrg?si=lz45BLmjS_7EOOGK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+})
+
+videoItem[2].addEventListener('click', function () {
+  if (this.classList.contains('active')){
+    return
+  }
+  this.classList.add('active')
+  popupTitle.innerHTML = 'Диспансерное наблюдение пациентов <nobr>с раком</nobr> мочевого пузыря после лечения'
+  popup.insertAdjacentHTML('beforeEnd', '<iframe src="https://www.youtube.com/embed/DHQtHpOyHuo?si=mnrria_W8EwlMeKS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+})
+
+close.addEventListener('click', function () {
+  mask.classList.add('hidden')
+  popup.classList.add('hidden')
   videoItem.forEach(el => {
-    el.addEventListener('click', function () {
-      mask.classList.remove('hidden')
-      popup.classList.remove('hidden')
-    })
+    el.classList.remove('active')
   })
 
-  videoItem[0].addEventListener('click', function () {
-    if (this.classList.contains('active')){
-      return
-    }
-    this.classList.add('active')
-    popupTitle.innerHTML = 'Лечение пациентов <nobr>с раком</nobr> мочевого пузыря'
-    popup.insertAdjacentHTML('beforeEnd', '<iframe src="https://www.youtube.com/embed/aqGwO4o1sks?si=ctc45fMah1QSuPg7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
-    const video = document.querySelector('.popup iframe')
-    if (video.requestFullscreen) {
-      video.requestFullscreen();
-    } else if (video.mozRequestFullScreen) {
-      video.mozRequestFullScreen();
-    } else if (video.webkitRequestFullscreen) {
-      video.webkitRequestFullscreen();
-    } else if (video.msRequestFullscreen) {
-      video.msRequestFullscreen();
-    }
-  })
-
-  videoItem[1].addEventListener('click', function () {
-    if (this.classList.contains('active')){
-      return
-    }
-    this.classList.add('active')
-    popupTitle.innerHTML = 'Жизнь пациента <nobr>с раком</nobr> мочевого пузыря после противоопухолевого лечения или во время него'
-    popup.insertAdjacentHTML('beforeEnd', '<iframe src="https://www.youtube.com/embed/4wkZtHJnzrg?si=lz45BLmjS_7EOOGK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
-  })
-
-  videoItem[2].addEventListener('click', function () {
-    if (this.classList.contains('active')){
-      return
-    }
-    this.classList.add('active')
-    popupTitle.innerHTML = 'Диспансерное наблюдение пациентов <nobr>с раком</nobr> мочевого пузыря после лечения'
-    popup.insertAdjacentHTML('beforeEnd', '<iframe src="https://www.youtube.com/embed/DHQtHpOyHuo?si=mnrria_W8EwlMeKS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
-  })
-
-  close.addEventListener('click', function () {
-    mask.classList.add('hidden')
-    popup.classList.add('hidden')
-    videoItem.forEach(el => {
-      el.classList.remove('active')
-    })
-
-    const video = document.querySelector('.popup iframe')
-    video.remove()
-  })
-
-
-// if (document.documentElement.clientWidth < 768) {
-//   videoItem[0].addEventListener('click', function () {
-//     videoItem[0].insertAdjacentHTML('beforeEnd', '<iframe src="https://www.youtube.com/embed/aqGwO4o1sks?si=ctc45fMah1QSuPg7?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
-//     const video = document.querySelector('.video__content-item-video iframe')
-//     if (video.requestFullscreen) {
-//       video.requestFullscreen();
-//     } else if (video.mozRequestFullScreen) {
-//       video.mozRequestFullScreen();
-//     } else if (video.webkitRequestFullscreen) {
-//       video.webkitRequestFullscreen();
-//     } else if (video.msRequestFullscreen) {
-//       video.msRequestFullscreen();
-//     }
-//   })
-// }
+  const video = document.querySelector('.popup iframe')
+  video.remove()
+})
